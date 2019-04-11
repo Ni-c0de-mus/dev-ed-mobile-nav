52f7a07c1e5a6c9326d989b658f18213b06b9c16
Ni-c0de-mus <44680555+Ni-c0de-mus@users.noreply.github.com>
2019-04-11 23:27:34 +0100

    Commit 4a

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav_links');
 
+
+    // Toggle Nav
+
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav_active');
     });
+
 }
 
+
+
+
 navSlide();
+
+
+// Alert
 
 alert("Hello! I am an alert box!!");
--- js/main2.js
+++ js/main2.js
@@ -1,1 +1,10 @@
 alert("Hello! I am an alert box!!");
+
+// Animate Links
+
+    const navLinks = document.QuerySelectorAll('.nav_links li');
+
+navLinks.forEach((link, index) => {
+    link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7}s'
+});
+console.log(index / 7);
