db843bd55aba0e759d3ae10c2266a4788eed5600
Ni-c0de-mus <44680555+Ni-c0de-mus@users.noreply.github.com>
2019-04-11 22:39:54 +0100

    Commit 4 Favicon

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav_active');
+    });
+}
+
+navSlide();
+
+alert("Hello! I am an alert box!!");
